72de7aa0cc6e126d085cc93914e6dca33e0852a8
Mareike Heuer <mareike.heuer@culture4life.de>
2022-10-28 12:58:32 +0200

    Add FeedbackForm component & handle input state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 import Header from "./components/Header";
+import FeedbackForm from "./components/FeedbackForm";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
@@ -18,6 +19,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
