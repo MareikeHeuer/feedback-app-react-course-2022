6bb967d0488e0bb0dc51a68ac7fce41fedc637e3
Mareike Heuer <mareike.heuer@culture4life.de>
2023-01-14 14:44:02 +0100

    Add react-router-dom package & AboutPage component

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from "../components/shared/Card";
+
+function AboutPage() {
+  return (
+    <div className="about">
+      <h1>About This Project</h1>
+      <p>This is a React app to leave feedback about a product or service</p>
+      <p>Version: 1.0.0</p>
+      <a href="/">Back to home</a>
+    </div>
+  );
+}
+
+export default AboutPage;
